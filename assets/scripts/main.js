e1c643c057a03b8b846e2abfa9082355d126387d
s1duan <43221167+s1duan@users.noreply.github.com>
2021-02-03 22:53:46 -0800

    text indicator

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,20 @@
 // main.js
 var audio = document.getElementById("horn-sound");
-var textIndicator = document.getElementById("volume-number").value;
+var textIndicator = document.getElementById("volume-number");
 var sliderBar = document.getElementById("volume-slider");
 
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector.apply('#volume-number').onchange = set_slider_bar_val;
+    document.querySelector('#volume-number').onchange = set_slider_bar_val;
+    document.querySelector('#volume-slider').onchange = set_text_indicator_val;
 }, false);
 
 function set_slider_bar_val(event){
     sliderBar.value = event.target.value;
+    audio.volume = event.target.value;
+}
+
+function set_text_indicator_val(event){
+    textIndicator.value = event.target.value;
     audio.volume = event.target.value;
 }
 // TODO
