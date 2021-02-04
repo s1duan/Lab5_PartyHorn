6f28349c3d4c78e8f14eb700edf1b389ba352845
s1duan <43221167+s1duan@users.noreply.github.com>
2021-02-03 22:36:31 -0800

    text indicator

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
 // main.js
-var audio = document.getElementById("myVideo");
+var audio = document.getElementById("horn-sound");
 var textIndicator = document.getElementById("volume-number").value;
 console.log(textIndicator);
 audio.volume = textIndicator;
