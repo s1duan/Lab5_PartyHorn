16e395f9b11e1fa61c864191cc200836cad35a85
s1duan <43221167+s1duan@users.noreply.github.com>
2021-02-04 23:13:14 -0800

    volume icons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,7 +7,7 @@ var airHorn = document.getElementById("radio-air-horn");
 var carHorn = document.getElementById("radio-car-horn");
 var partyHorn = document.getElementById("radio-party-horn");
 var button = document.getElementById("honk-btn");
-/*
+
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#volume-number').onchange = set_slider_bar_val;
     document.querySelector('#volume-slider').onchange = set_text_indicator_val;
@@ -16,16 +16,11 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#radio-party-horn').onchange = set_party_horn;
     document.querySelector('#honk-btn').onclick = function() { audio.play() }
 }, false);
-*/
-textIndicator.onchange = set_slider_bar_val();
-sliderBar.onchange = set_text_indicator_val();
-airHorn.onchange = set_air_horn();
-carHorn.onchange = set_car_horn();
-partyHorn.onchange = set_party_horn();
+
 button.onclick = function() { audio.play() }
 
 function set_button(value){
-    button,disabled = false;
+    button.disabled = false;
     if (value >= 67) {
         volumeImg.src = "assets/media/icons/volume-level-3.svg"
     }
@@ -40,34 +35,34 @@ function set_button(value){
         button.disabled = true
     }
 }
-function set_slider_bar_val(){
-    var value = textIndicator.value
+function set_slider_bar_val(event){
+    var value = event.target.value
     sliderBar.value = value;
     audio.volume = value/100.0;
     set_button(value);
 }
 
-function set_text_indicator_val(){
-    var value = sliderBar.value;
+function set_text_indicator_val(event){
+    var value = event.target.value;
     textIndicator.value = value;
     audio.volume = value/100.0;
     set_button(value);
 }
 
-function set_air_horn(){
-    if (airHorn.value){
+function set_air_horn(event){
+    if (event.target.value){
         audio.src = "assets/media/audio/air-horn.mp3"
     }
 }
 
-function set_car_horn(){
-    if (carHorn.value){
+function set_car_horn(event){
+    if (event.target.value){
         audio.src = "assets/media/audio/car-horn.mp3"
     }
 }
 
-function set_party_horn(){
-    if (partyHorn.value){
+function set_party_horn(event){
+    if (event.target.value){
         audio.src = "assets/media/audio/party-horn.mp3"
     }
 }
